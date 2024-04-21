4329ec1dfec5dd336f8bcbd13f2509ac84c3129a
Lamia Moon <108029429+Lamia-moon@users.noreply.github.com>
2024-04-21 11:50:53 +0000

    conversion of datatypes

---
+++ 0_basics/3_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" = 33
+// "33b" = NaN
+// true = 1 or false = 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1= true, 0 = false
+// "Moon" = true
+// "" = false 
+
+let someNumber = 77
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
