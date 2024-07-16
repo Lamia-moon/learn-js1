f8b54c5de7e62c3c3bacf7959af596b9a134116a
Lamia Moon <108029429+Lamia-moon@users.noreply.github.com>
2024-07-16 13:42:48 +0000

    container config done

--- 0_basics/datatypes-summary.js
+++ 0_basics/datatypes-summary.js
@@ -1,30 +1,48 @@
 //  Primitive
 //  7 types: string, Number, boolean, null, undefined, symbol, bigInt
 
-const score= 100
-const scoreValue = 100.3
+// const score= 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let eserEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let eserEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id === anotherId);
-// output false
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+// // output false
 
-const bigNumber = 1111111111122222222223333333n
+// const bigNumber = 1111111111122222222223333333n
 
-// Reference (Non primitive)
-//  Array, Objects, Functions
+// // Reference (Non primitive)
+// //  Array, Objects, Functions
 
-const color = [ "red", "blue", "black"];
-let myObj = {
-    name: "moon",
-    age: 26,
-    color: fair
+// const color = [ "red", "blue", "black"];
+// let myObj = {
+//     name: "moon",
+//     age: 26,
+// }
+
+// const myFunction = function () {
+//     console.log("Hello World");
+// }
+// console.log(typeof anotherId);
+
+// ***************************************************************
+//  stack(primitive) ||  heap(non primitive)
+
+let myMainName = "Lamia"
+let anotherName = myMainName
+anotherName = "Moon"
+console.log(myMainName);
+console.log(anotherName);
+
+let userOne = {
+    email : "abcd@gmail.com"
 }
+let userTwo = userOne
+userTwo.email = "bbccdd@gmail.com"
 
-const myFunction = function () {
-    console.log("Hello World");
-}
+console.log(userOne.email);
+console.log(userTwo.email);
