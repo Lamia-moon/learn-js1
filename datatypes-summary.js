f1ed282683f9bbfa1829f6a9c760f4d505402ef4
Lamia Moon <108029429+Lamia-moon@users.noreply.github.com>
2024-07-16 07:26:26 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,30 @@
+//  Primitive
+//  7 types: string, Number, boolean, null, undefined, symbol, bigInt
+
+const score= 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let eserEmail;
+
+const id = Symbol('123')
+console anotherId = Symbol('123')
+console.log(id === anotherId);
+// output false
+
+const bigNumber = 1111111111122222222223333333n
+
+// Reference (Non primitive)
+//  Array, Objects, Functions
+
+const color = [ "red", "blue", "black"];
+let myObj = {
+    name: "moon",
+    age: 26,
+    color: fair
+}
+
+const myFunction = function () {
+    console.log("Hello World");
+}
