872b87684b8ec0e59f5b3710f5b528437317d26b
Lamia Moon <108029429+Lamia-moon@users.noreply.github.com>
2024-07-30 06:38:11 +0000

    while, do while

---
+++ 5_iterations/while.js
@@ -0,0 +1,24 @@
+// while
+
+// let index = 1
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index= index + 2
+// }
+
+// let myArr = ['red', 'blue', 'green']
+// let arr = 0
+
+//  while (arr < myArr.length) {
+//     console.log(`Value is ${myArr}`);
+//     arr ++
+//  }
+
+
+// do while
+
+ let val = 10
+ do {
+    console.log(`Value is ${val}`);
+    val ++
+ } while (val <= 10);
